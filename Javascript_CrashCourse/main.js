9454bf2e9215f7d3296d909a4ef149a114a9674d
kukjinkim <tlaruddo2@gmail.com>
2022-12-28 16:06:29 -0800

    [Javascript] ~ loop

--- Javascript_CrashCourse/main.js
+++ Javascript_CrashCourse/main.js
@@ -81,6 +81,101 @@ console.log(Array.isArray(fruits));
 // // Get index
 console.log(fruits.indexOf('oranges'));
 
-//30m ~ 
-// https://www.youtube.com/watch?v=hdI2bqOjy3c&t=18s
-ss
+
+// OBJECT LITERALS
+// Key, value pair
+const person = {
+    firstName: 'John',
+    lastName: 'Tom',
+    age: 30,
+    hobbies: ['music', 'movies', 'sports'],
+    // address is object 
+    address: {
+      street: '50 Main st',
+      city: 'Boston',
+      state: 'MA'
+    }
+  }
+  
+  // Get single value
+  console.log(person.firstName)
+
+  // Get two value
+  console.log(person.firstName, person.lastName)  
+  
+  // Get array value
+  console.log(person.hobbies[1]);
+  
+  // Get embedded object
+  console.log(person.address.city);
+
+  //polling out value (ES6)
+  const { firstName, lastname, address:{city}} = person;
+  console.log(firstName) //John 
+  console.log(city) //Boston
+
+  // Add property
+  person.email = 'jdoe@gmail.com';
+  
+  // Array of objects
+  const todos = [
+    {
+      id: 1,
+      text: 'Take out trash',
+      isComplete: false
+    },
+    {
+      id: 2,
+      text: 'Dinner with wife',
+      isComplete: false
+    },
+    {
+      id: 3,
+      text: 'Meeting with boss',
+      isComplete: true
+    }
+  ];
+  
+  // Get specific object value
+  console.log(todos[1].text);
+  
+  // Format as JSON
+  // When send data to server
+  const todoJson = Json.stringify(todos);
+  console.log(todoJson);
+  
+  
+  // LOOPS
+  
+  // For
+  for(let i = 0; i <= 10; i++){
+    console.log(`For Loop Number: ${i}`);
+  }
+  
+  // While
+  let i = 0
+  while(i <= 10) {
+    console.log(`While Loop Number: ${i}`);
+    i++;
+  }
+  
+  // Loop Through Arrays
+  // For Loop
+  for(let i = 0; i < todos.length; i++){
+    console.log(` Todo ${i + 1}: ${todos[i].text}`);
+  }
+  
+  // For...of Loop
+  for(let todo of todos) {
+    console.log(todo.text);
+  }
+  
+  
+  // HIGH ORDER ARRAY METHODS (show prototype)
+  // Suggestion way to iteration 
+  // forEach, map, filter 
+  
+  //////////43:00 https://www.youtube.com/watch?v=hdI2bqOjy3c&t=18s
+  ////////////
+
+  
