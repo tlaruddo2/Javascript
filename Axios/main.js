c851477a0830ebfe7f22909d3a698db2762b1c33
kukjinkim <tlaruddo2@gmail.com>
2023-01-02 10:54:27 -0800

    .

--- Axios/main.js
+++ Axios/main.js
@@ -1,4 +1,5 @@
-import axios from 'axios';
+// import axios from 'axios';
+const axios = require('axios');
 // AXIOS GLOBALS
 // default header
 axios.defaults.headers.common['X-Auth-Token'] = //ex. JWT from JWT.io
